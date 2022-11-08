0f83bf51aece03f068c17f3a9ff6ec37bd9ff0df
Ekaterina <105393600+Kate-Tikh@users.noreply.github.com>
2022-11-08 17:43:15 +0300

    Add files via upload

---
+++ components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Button from "../Button/Button";
+import './Header.css';
+import {useTelegram} from "../../hook/useTelegram";
+import './Header.css';
+
+const Header = () => {
+    const {user, onClose} = useTelegram();
+
+    return (
+        <div className={'header'}>
+            <Button onClick={onClose}>Close</Button>
+            <span className={'username'}>
+                {user?.username}
+            </span>
+        </div>
+    );
+};
+
+export default Header;
